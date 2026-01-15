10e4495c98a72b2032289714fd59aaf36f08c9be
GAYATHRISHRIVARSITHAAY <gayathrishrivarsithaa2004@gmail.com>
2026-01-15 15:39:37 +0530

    Update Kotlin_oops_Day5.kt

--- Kotlin_oops_Day5.kt
+++ Kotlin_oops_Day5.kt
@@ -58,6 +58,100 @@ class Child1:Student,Father{
         println("Child")
     }
 }
+//using final keyword in override
+open class Food{
+    //this functions should be open to override in the future
+    open fun print(){
+        println("Food")
+    }
+}
+open class Fruit:Food(){
+    final override fun print(){
+        println("Fruit")
+    }
+}
+class Fruitchild:Fruit(){
+    //this gives error cause the function print is final in Fruit
+    // override fun print(){
+    //     println("Friutchild")
+    // }
+}
+//handling diamond problem
+interface A{
+    fun show(){
+        println("A")
+    }
+}
+interface B:A{
+    override fun show(){
+        println("B")
+    }
+}
+interface C:A{
+    override fun show(){
+        println("C")
+    }
+}
+class D:B,C{
+    override fun show(){
+        //we cannot do this super<A>.show because A is not immediate super class for D
+        //it will be an error
+        super<B>.show()
+        super<C>.show()
+    }
+}
+//this keyword
+open class This_Class(val name:String,val age:Int){
+    fun info(name:String){
+        println("Given name is:$name")
+        println("Already given name is:${this.name}")
+    }
+}
+
+//super keyword
+//accessing parent method
+open class Parent_Super1{
+    open fun disp(){
+        println("Parent class")
+    }
+}
+class Child_Super1:Parent_Super1(){
+    override fun disp(){
+        println("Child Class")
+        super.disp()
+    }
+}
+//accessing parent property
+open class Parent_Super2{
+    open val color="Pink"
+}
+class Child_Super2:Parent_Super2(){
+    override val color="Blue"
+    fun disp(){
+        println("Child color:$color")
+        println("Parent color:${super.color}")
+    }
+}
+//polymorphism using interface
+interface Animal{
+    abstract fun sound()
+}
+class Dog:Animal{
+    override fun sound(){
+        println("Bark")
+    }
+}
+class Cat:Animal{
+    override fun sound(){
+        println("Meow")
+    }
+}
+//upcasting
+//storing a child object inside a parent refernce
+
+//downcasting
+//converting a parent reference back into a child refernce
+
 fun main(){
     val child=Child()
     child.read()
@@ -72,4 +166,18 @@ fun main(){
     child1.care()
     child1.study()
     child1.child()
+    val fruitchild=Fruitchild()
+    fruitchild.print()
+    val d=D()
+    d.show()
+    val child_super1=Child_Super1()
+    child_super1.disp()
+    val child_super2=Child_Super2()
+    child_super2.disp()
+    val this_class=This_Class("Gayathri",21)
+    this_class.info("Shri")
+    val dog=Dog()
+    dog.sound()
+    val cat=Cat()
+    cat.sound()
 }
